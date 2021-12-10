7b33278a38a67ab50257905bfff3b1b71f9989c1
Dave <389788728@qq.com>
2021-12-10 10:17:19 +0800

    Update main.go
    update

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, Worl!")
+		return c.String(http.StatusOK, "Hello, World!")
 	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
